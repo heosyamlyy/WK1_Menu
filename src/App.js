1f54b49885fbc76071193458384991861c9eab90
Yangyang Liu <heosyamlyy@gmail.com>
2019-09-18 00:04:44 -0700

    Basic Function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import { Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponents'
+import './App.css';
+import {DISHES} from './shared/dishes';
+import { tsConstructorType } from '@babel/types';
+
+
+class App extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+
+
+
+
+  render() {
+
+      
+    return (
+      <div className="App">
+        <Navbar dark color = "primary">
+          <div className = "container">
+            <NavbarBrand href="/">Ristonrante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes ={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
